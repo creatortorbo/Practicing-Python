536b6d2f5f12f37c4ffb3e3e74840c95a8bcd12a
creater toro <bablupandey446@gmil.com>
2024-05-17 08:47:13 +0530

    two code partices the code

---
+++ add to no.py
@@ -0,0 +1,5 @@
+# here add the tow number 
+a = int(input('Enter a number:'))
+b = int(input('Enter a number:'))
+a = a + b 
+print('Sum of a + b :',a)
---
+++ squre_root.py
@@ -0,0 +1,13 @@
+# here we finding the square root of the any number
+a = int(input('number a number :'))
+
+b = a**(1/2)
+# b = a/2
+print('squre root :',b)
+
+
+# # here using the math model 
+# import math 
+# num = int(input('Enter a number here :'))
+# sr = math.sqrt(num)
+# print('the square root of the given number is',sr)

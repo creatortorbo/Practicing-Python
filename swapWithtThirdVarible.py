e6a9b9106ac3e8e45ace9d464ac1a4707ab33abf
creater toro <bablupandey446@gmil.com>
2024-05-18 16:08:28 +0530

    swaping without third varible

---
+++ swapWithtThirdVarible.py
@@ -0,0 +1,8 @@
+# here using the swaping value by no third varible
+a  = int(input("Enter a first number:"))
+b = int(input("Enter a second number:"))
+
+a,b = b,a 
+
+print('printing the values of the a:',a)
+print('priting the values of the b:',b)

301282e86637f131a013222f4aa5349b82862a1a
creater toro <bablupandey446@gmil.com>
2024-05-18 16:28:33 +0530

    converting the kilometer to miles

---
+++ kilometer to miles.py
@@ -0,0 +1,6 @@
+# here converting into the kilomerter to miles 
+kilometers = int(input('Enter a kilometers :'))
+
+miles = kilometers * 0.621371
+
+print('It conveted kilometers to miles :',miles,'miles')

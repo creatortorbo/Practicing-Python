2761aefc977ce2685d328ee61bba4bc7e3e92834
creater toro <bablupandey446@gmil.com>
2024-05-21 17:23:40 +0530

    converting_celsiusOfFahrenheit

---
+++ converting_celsiusOfFahrenheit.py
@@ -0,0 +1,4 @@
+# Python Program to Convert Celsius To Fahrenheit
+Celsius  = int(input("Enter a number :"))
+fahrenheit = (9/5*Celsius) + 32
+print("The converting Celsius to Fahrenheit is ",fahrenheit)

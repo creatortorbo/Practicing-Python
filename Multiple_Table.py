0cddadd9e0cc4abfd612ac5ebf983b8e0b9c21e5
creater toro <bablupandey446@gmil.com>
2024-05-22 16:03:57 +0530

    multiple table

---
+++ Multiple_Table.py
@@ -0,0 +1,11 @@
+# Display the Multiplication Table using for loops 
+# num = 7
+# for i in range(1,11):
+
+#  print(num,"X",i,"=",num*1)   
+
+# here using the user input 
+num = int(input('Enter a number :'))
+
+for i in range(1,11):
+ print(num,"X",i,"=",num*i)

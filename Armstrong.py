a077259fc5d15e3f24115b96e3ad0f93f2d14cab
creater toro <bablupandey446@gmil.com>
2024-05-24 07:00:57 +0530

    Armstrong number

---
+++ Armstrong.py
@@ -0,0 +1,17 @@
+# Python Program to Check If the Number is Armstrong or Not?
+num = int(input('Enter a number is :'))
+
+sum = 0
+temp = num 
+
+while temp >0:
+   digit = temp%10
+   cube = digit ** 3
+   sum = sum + cube 
+   temp//= 10
+
+
+if sum == num :
+   print('it is an armstrong number')
+else:
+   print('it is  not aramstrong number')

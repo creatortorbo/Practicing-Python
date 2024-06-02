ea93ef683cda179dc9450a438c6c26118001b342
creater toro <bablupandey446@gmil.com>
2024-06-02 10:44:00 +0530

    strip for the space remove from the string

---
+++ code_Rev/strip.py
@@ -0,0 +1,5 @@
+# here using the strip() for removing the space 
+# name = str(input("Enter a name :"))
+name = "    bablukumarpandey     "
+
+print('The strip is space in the string :',name.strip())

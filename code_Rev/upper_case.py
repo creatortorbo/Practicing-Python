9b6a1c1edd4f8f01364d2be29346e3b9280ed214
creater toro <bablupandey446@gmil.com>
2024-06-02 10:23:27 +0530

    upper case

---
+++ code_Rev/upper_case.py
@@ -0,0 +1,8 @@
+# here wap for the upper case 
+# a = 'my name is bablu'
+# print(a.upper())
+
+# using with user input 
+name = str(input('Enter a full name :'))
+
+print('The string converted into the upper case :',name.upper())

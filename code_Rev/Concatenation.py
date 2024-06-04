476d6b3709f0261d4ff5dd564fbf243db3399098
creater toro <bablupandey446@gmil.com>
2024-06-04 17:54:16 +0530

    String Concatenation

---
+++ code_Rev/Concatenation.py
@@ -0,0 +1,10 @@
+# here using String Concatenation
+# name1= 'vishal'
+# name2 = 'Aman'
+# print("here add the two string in this :",name1+name2)
+name_fist = str(input('Enter a fist name :'))
+name_second  = str(input('Enter a second name :'))
+print("here add the two string in this :",name_fist+name_second)
+
+
+

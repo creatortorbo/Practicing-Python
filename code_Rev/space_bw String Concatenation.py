1b690950bffed1a29fc3f01850392a9860bd3bb9
creater toro <bablupandey446@gmil.com>
2024-06-04 18:16:32 +0530

    space with sting conc

---
+++ code_Rev/space_bw String Concatenation.py
@@ -0,0 +1,6 @@
+# here we add the 2 string with using with space 
+name_fist = str(input('Enter a fist_name :'))
+name_second = str(input('Enter a second :'))
+
+conc  = name_fist + " " + name_second 
+print('The sum of two string with the space :',conc )
